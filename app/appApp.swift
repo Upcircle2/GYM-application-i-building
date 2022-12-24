33c3efa30e26ddfd69656f01ced7759319fd2f31
sangwon <epepep0938@gmail.com>
2022-12-24 12:11:31 +0900

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by sswv on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
